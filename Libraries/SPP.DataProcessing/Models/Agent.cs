f9c839d6fca7e0ad0ee47fdbe40c2dcb05322a1e
mmariamoldovan <moldovanmariasilviuta@gmail.com>
2025-05-15 22:24:28 +0300

    Update project files and models

--- Libraries/SPP.DataProcessing/Models/Agent.cs
+++ Libraries/SPP.DataProcessing/Models/Agent.cs
@@ -1,13 +1,14 @@
+
 public class Agent
 {
-     public int AgentID{ get; set; }
-    public required string Name{ get; set; }
-    public required string CodeName{ get; set; }
-    public required string Location{ get; set; }
-    public DateOnly DateOfBirth{ get; set; }
-    public required string Contact{ get; set; }
-    public required string LastMission{ get; set; }
-    public required string Status{ get; set; }
-    public required string ClearenceLevel{ get; set; }
-    public required string ArchiveNote{ get; set; }
+    public int AgentID { get; set; }
+    public required string Name { get; set; }
+    public required string CodeName { get; set; }
+    public required string Location { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public required string Contact { get; set; }
+    public required string LastMission { get; set; }
+    public required string Status { get; set; }
+    public required string ClearenceLevel { get; set; }
+    public required string ArchiveNote { get; set; }
 }
